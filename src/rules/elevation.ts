3addd3f064dc378e34cde368364236ef43e5d8c7
J-Sek <J-Sek@users.noreply.github.com>
2026-02-26 16:09:38 +0100

    feat: built-in elevation presets for MD2 and MD3 (#8)
    * feat: built-in elevation presets for MD2 and MD3
    * chore: use kebabCase for keys from custom rules
    * chore: missing commas

--- src/rules/elevation.ts
+++ src/rules/elevation.ts
@@ -1,12 +1,28 @@
 import type { Rule } from 'unocss'
-import { elevations } from '../theme'
+import { elevationPresets } from '../theme'
+import { kebabCase } from '../utils'
 
-export function elevationRules (): Rule[] {
+export type ElevationOptions = 'md2' | 'md3' | Record<string, Record<string, string>>
+
+export function elevationRules (elevation: ElevationOptions = 'md3'): Rule[] {
+  const preset = elevation === 'md2'
+    ? elevationPresets.md2
+    : (elevation === 'md3'
+        ? elevationPresets.md3
+        : elevation)
+
   const rules: Rule[] = []
 
-  for (let i = 0; i <= 24; i++) {
-    rules.push([`elevation-${i}`, { 'box-shadow': elevations[i] }])
+  for (const [level, styles] of Object.entries(preset)) {
+    const css: Record<string, string | number> = Object.entries(styles)
+      .reduce(
+        (o, [key, value]) => ({ ...o, [kebabCase(key)]: value }),
+        {} as Record<string, string>,
+      )
+    rules.push([`elevation-${level}`, css])
   }
+
+  rules.push(['elevation-overlay', { 'background-image': 'linear-gradient(var(--v-elevation-overlay), var(--v-elevation-overlay))' }])
 
   return rules
 }
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,3 +1,4 @@
+import type { ElevationOptions } from '../rules/elevation'
 import type { TypographyOptions } from '../rules/typography'
 
 /* eslint-disable unicorn/numeric-separators-style */
@@ -69,33 +70,62 @@ export const typographyPresets: { md2: TypographyPreset, md3: TypographyPreset }
   },
 }
 
-export const elevations: string[] = [
-  'none',
-  '0px 2px 1px -1px rgba(0,0,0,.2), 0px 1px 1px 0px rgba(0,0,0,.14), 0px 1px 3px 0px rgba(0,0,0,.12)',
-  '0px 3px 1px -2px rgba(0,0,0,.2), 0px 2px 2px 0px rgba(0,0,0,.14), 0px 1px 5px 0px rgba(0,0,0,.12)',
-  '0px 3px 3px -2px rgba(0,0,0,.2), 0px 3px 4px 0px rgba(0,0,0,.14), 0px 1px 8px 0px rgba(0,0,0,.12)',
-  '0px 2px 4px -1px rgba(0,0,0,.2), 0px 4px 5px 0px rgba(0,0,0,.14), 0px 1px 10px 0px rgba(0,0,0,.12)',
-  '0px 3px 5px -1px rgba(0,0,0,.2), 0px 5px 8px 0px rgba(0,0,0,.14), 0px 1px 14px 0px rgba(0,0,0,.12)',
-  '0px 3px 5px -1px rgba(0,0,0,.2), 0px 6px 10px 0px rgba(0,0,0,.14), 0px 1px 18px 0px rgba(0,0,0,.12)',
-  '0px 4px 5px -2px rgba(0,0,0,.2), 0px 7px 10px 1px rgba(0,0,0,.14), 0px 2px 16px 1px rgba(0,0,0,.12)',
-  '0px 5px 5px -3px rgba(0,0,0,.2), 0px 8px 10px 1px rgba(0,0,0,.14), 0px 3px 14px 2px rgba(0,0,0,.12)',
-  '0px 5px 6px -3px rgba(0,0,0,.2), 0px 9px 12px 1px rgba(0,0,0,.14), 0px 3px 16px 2px rgba(0,0,0,.12)',
-  '0px 6px 6px -3px rgba(0,0,0,.2), 0px 10px 14px 1px rgba(0,0,0,.14), 0px 4px 18px 3px rgba(0,0,0,.12)',
-  '0px 6px 7px -4px rgba(0,0,0,.2), 0px 11px 15px 1px rgba(0,0,0,.14), 0px 4px 20px 3px rgba(0,0,0,.12)',
-  '0px 7px 8px -4px rgba(0,0,0,.2), 0px 12px 17px 2px rgba(0,0,0,.14), 0px 5px 22px 4px rgba(0,0,0,.12)',
-  '0px 7px 8px -4px rgba(0,0,0,.2), 0px 13px 19px 2px rgba(0,0,0,.14), 0px 5px 24px 4px rgba(0,0,0,.12)',
-  '0px 7px 9px -4px rgba(0,0,0,.2), 0px 14px 21px 2px rgba(0,0,0,.14), 0px 5px 26px 4px rgba(0,0,0,.12)',
-  '0px 8px 9px -5px rgba(0,0,0,.2), 0px 15px 22px 2px rgba(0,0,0,.14), 0px 6px 28px 5px rgba(0,0,0,.12)',
-  '0px 8px 10px -5px rgba(0,0,0,.2), 0px 16px 24px 2px rgba(0,0,0,.14), 0px 6px 30px 5px rgba(0,0,0,.12)',
-  '0px 8px 11px -5px rgba(0,0,0,.2), 0px 17px 26px 2px rgba(0,0,0,.14), 0px 6px 32px 5px rgba(0,0,0,.12)',
-  '0px 9px 11px -5px rgba(0,0,0,.2), 0px 18px 28px 2px rgba(0,0,0,.14), 0px 7px 34px 6px rgba(0,0,0,.12)',
-  '0px 9px 12px -6px rgba(0,0,0,.2), 0px 19px 29px 2px rgba(0,0,0,.14), 0px 7px 36px 6px rgba(0,0,0,.12)',
-  '0px 10px 13px -6px rgba(0,0,0,.2), 0px 20px 31px 3px rgba(0,0,0,.14), 0px 8px 38px 7px rgba(0,0,0,.12)',
-  '0px 10px 13px -6px rgba(0,0,0,.2), 0px 21px 33px 3px rgba(0,0,0,.14), 0px 8px 40px 7px rgba(0,0,0,.12)',
-  '0px 10px 14px -6px rgba(0,0,0,.2), 0px 22px 35px 3px rgba(0,0,0,.14), 0px 8px 42px 7px rgba(0,0,0,.12)',
-  '0px 11px 14px -7px rgba(0,0,0,.2), 0px 23px 36px 3px rgba(0,0,0,.14), 0px 9px 44px 8px rgba(0,0,0,.12)',
-  '0px 11px 15px -7px rgba(0,0,0,.2), 0px 24px 38px 3px rgba(0,0,0,.14), 0px 9px 46px 8px rgba(0,0,0,.12)',
-]
+const elevationOverlayStep = 2 // %
+
+type ElevationPreset = Record<string, Record<string, string>>
+export const elevationPresets: { md2: ElevationPreset, md3: ElevationPreset } = {
+  md2: Object.fromEntries([
+    '0px  0px  0px  0px [1], 0px  0px  0px 0px [2], 0px 0px  0px 0px [3]',
+    '0px  2px  1px -1px [1], 0px  1px  1px 0px [2], 0px 1px  3px 0px [3]',
+    '0px  3px  1px -2px [1], 0px  2px  2px 0px [2], 0px 1px  5px 0px [3]',
+    '0px  3px  3px -2px [1], 0px  3px  4px 0px [2], 0px 1px  8px 0px [3]',
+    '0px  2px  4px -1px [1], 0px  4px  5px 0px [2], 0px 1px 10px 0px [3]',
+    '0px  3px  5px -1px [1], 0px  5px  8px 0px [2], 0px 1px 14px 0px [3]',
+    '0px  3px  5px -1px [1], 0px  6px 10px 0px [2], 0px 1px 18px 0px [3]',
+    '0px  4px  5px -2px [1], 0px  7px 10px 1px [2], 0px 2px 16px 1px [3]',
+    '0px  5px  5px -3px [1], 0px  8px 10px 1px [2], 0px 3px 14px 2px [3]',
+    '0px  5px  6px -3px [1], 0px  9px 12px 1px [2], 0px 3px 16px 2px [3]',
+    '0px  6px  6px -3px [1], 0px 10px 14px 1px [2], 0px 4px 18px 3px [3]',
+    '0px  6px  7px -4px [1], 0px 11px 15px 1px [2], 0px 4px 20px 3px [3]',
+    '0px  7px  8px -4px [1], 0px 12px 17px 2px [2], 0px 5px 22px 4px [3]',
+    '0px  7px  8px -4px [1], 0px 13px 19px 2px [2], 0px 5px 24px 4px [3]',
+    '0px  7px  9px -4px [1], 0px 14px 21px 2px [2], 0px 5px 26px 4px [3]',
+    '0px  8px  9px -5px [1], 0px 15px 22px 2px [2], 0px 6px 28px 5px [3]',
+    '0px  8px 10px -5px [1], 0px 16px 24px 2px [2], 0px 6px 30px 5px [3]',
+    '0px  8px 11px -5px [1], 0px 17px 26px 2px [2], 0px 6px 32px 5px [3]',
+    '0px  9px 11px -5px [1], 0px 18px 28px 2px [2], 0px 7px 34px 6px [3]',
+    '0px  9px 12px -6px [1], 0px 19px 29px 2px [2], 0px 7px 36px 6px [3]',
+    '0px 10px 13px -6px [1], 0px 20px 31px 3px [2], 0px 8px 38px 7px [3]',
+    '0px 10px 13px -6px [1], 0px 21px 33px 3px [2], 0px 8px 40px 7px [3]',
+    '0px 10px 14px -6px [1], 0px 22px 35px 3px [2], 0px 8px 42px 7px [3]',
+    '0px 11px 14px -7px [1], 0px 23px 36px 3px [2], 0px 9px 44px 8px [3]',
+    '0px 11px 15px -7px [1], 0px 24px 38px 3px [2], 0px 9px 46px 8px [3]',
+  ]
+    .map(line => line
+      .replace('[1]', 'var(--v-shadow-key-umbra-opacity,    rgba(0, 0, 0, 0.2 ))')
+      .replace('[2]', 'var(--v-shadow-key-penumbra-opacity, rgba(0, 0, 0, 0.14))')
+      .replace('[3]', 'var(--v-shadow-key-ambient-opacity,  rgba(0, 0, 0, 0.12))'),
+    )
+    .map((line, i) => [String(i), { 'box-shadow': line }]),
+  ),
+  md3: Object.fromEntries([
+    '0px 0px 0px 0px [1], 0px 0px  0px 0px [2]',
+    '0px 1px 2px 0px [1], 0px 1px  3px 1px [2]',
+    '0px 1px 2px 0px [1], 0px 2px  6px 2px [2]',
+    '0px 1px 3px 0px [1], 0px 4px  8px 3px [2]',
+    '0px 2px 3px 0px [1], 0px 6px 10px 4px [2]',
+    '0px 4px 4px 0px [1], 0px 8px 12px 6px [2]',
+  ]
+    .map(line => line
+      .replace('[1]', 'rgba(var(--v-shadow-color), var(--v-shadow-key-opacity,     0.3 ))')
+      .replace('[2]', 'rgba(var(--v-shadow-color), var(--v-shadow-ambient-opacity, 0.15))'),
+    )
+    .map((line, i) => [String(i), {
+      'box-shadow': line,
+      '--v-elevation-overlay': `color-mix(in srgb, var(--v-elevation-overlay-color, #000) ${i * elevationOverlayStep}%, transparent)`,
+    }]),
+  ),
+}
 
 export type RuleGroup
   = | 'borders'
@@ -119,6 +149,7 @@ export interface PresetVuetifyOptions {
   spacer?: number
   spacerSteps?: number
   typography?: TypographyOptions
+  elevation?: ElevationOptions
   breakpoints?: Record<string, string>
   important?: boolean
   font?: Record<string, string>
